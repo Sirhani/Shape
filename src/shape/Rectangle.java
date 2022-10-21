69db07f1334c227917b353738e6df24fb804d610
Sirhani <115448982+Sirhani@users.noreply.github.com>
2022-10-22 00:30:06 +0300

    print perimeter

--- src/shape/Rectangle.java
+++ src/shape/Rectangle.java
@@ -17,7 +17,9 @@ public class Rectangle {
     public int getArea(int x, int y) { 
         return x*y; // write code to calculate area here
     } 
-    
+    public int getPerimeter(int length, int width){
+    int perimeter = 2 * (length + width);
+    return perimeter;
     // write code to calculate perimeter here
     // getPerimeter()
     
