91b1b34c120df1ff771c5a918834026dd23dab11
Sirhani <115448982+Sirhani@users.noreply.github.com>
2022-10-22 00:24:14 +0300

    added a getPerimeter method that calculates the perimeter of a rectangle

--- src/shape/Rectangle.java
+++ src/shape/Rectangle.java
@@ -17,6 +17,10 @@ public class Rectangle {
     public int getArea(int x, int y) { 
         return x*y; // write code to calculate area here
     } 
+    public int getPerimeter(int length, int width){
+    int perimeter = 2 * (length + width);
+    return perimeter;
+    }
     
     // write code to calculate perimeter here
     // getPerimeter()
